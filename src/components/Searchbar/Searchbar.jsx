3fd11eea5b43037b8ead2d6c7063d2b310dffaac
Metaldetect <stas.klymenko@icloud.com>
2023-08-06 10:20:39 +0200

    Searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  SearchbarHead,
+  SearchbarForm,
+  SearchbarInput,
+  SearchbarButton,
+} from './SearchbarStyles';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!this.state.query.trim()) {
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <SearchbarHead>
+        <SearchbarForm onSubmit={this.handleSubmit}>
+          <SearchbarButton type="submit">Sabmit</SearchbarButton>
+
+          <SearchbarInput
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search..."
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </SearchbarForm>
+      </SearchbarHead>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
---
+++ src/components/Searchbar/SearchbarStyles.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const SearchbarHead = styled.div``;
+export const SearchbarForm = styled.form``;
+export const SearchbarInput = styled.input``;
+export const SearchbarButton = styled.button``;
